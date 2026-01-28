85a5fa28b764f33564b55a0cfcbbca5e2777ab43
jasnoorkaur15 <kjasnoor15@gmail.com>
2026-01-28 20:20:17 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+# load dataset
+df = pd.read_csv("../data/opd_data.csv")
+
+# basic checks
+print("First 5 rows:")
+print(df.head())
+
+print("\nDataset info:")
+print(df.info())
+
+print("\nMissing values:")
+print(df.isnull().sum())
 
